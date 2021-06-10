4903f0e6e6fbfaf9de13e99b9dc8e437e9855e46
tommy <tommy3321@live.com>
2021-06-10 14:18:20 +0800

    basic auth

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,9 +2,10 @@
   <div class="container">
     <!-- Branding Image -->
     <a class="navbar-brand " href="{{ url('/') }}">
-      Laravel Shop
+      LShop
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
@@ -17,8 +18,28 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
-        <li class="nav-item"><a class="nav-link" href="#">登入</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">註冊</a></li>
+        <!-- login start -->
+        @guest
+          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登入</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">註冊</a></li>
+        @else
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+               aria-haspopup="true" aria-expanded="false">
+{{--              <img src=""--}}
+{{--                   class="img-responsive img-circle" width="30px" height="30px">--}}
+              {{ Auth::user()->name }}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" id="logout" href="#"
+                 onclick="event.preventDefault();document.getElementById('logout-form').submit();">登出</a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+              </form>
+            </div>
+          </li>
+      @endguest
+      <!-- login end -->
       </ul>
     </div>
   </div>
