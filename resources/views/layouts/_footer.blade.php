7f6fe28f08e120f1c7e63d81a3fce4d293320ab6
tommy <tommy3321@live.com>
2021-06-10 14:08:26 +0800

    環境基本配置

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+       <a href="https://leo108.com" target="_blank">tommy</a> design <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:tommy3321@live.com">contact us</a></p>
+  </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', '首頁')
+
+@section('content')
+  <h1>hello</h1>
+@stop
