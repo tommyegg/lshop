274c392eb6ffcc729ed65bc7284bdb2ef0446bbd
tommy <tommy3321@live.com>
2021-06-13 19:57:38 +0800

    product list - add info

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -32,12 +32,16 @@
               <div class="col-3 product-item">
                 <div class="product-content">
                   <div class="top">
-                    <div class="img"><img  src="{{ $product->image_url }}" alt=""></div>
+                    <a href="{{ route('products.show', ['product' => $product->id]) }}">
+                      <img src="{{ $product->image_url }}" alt="">
+                    </a>
                     <div class="price"><b>$</b>{{ $product->price }}</div>
-                    <div class="title">{{ $product->title }}</div>
+                    <div class="title">
+                      <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
+                    </div>
                   </div>
                   <div class="bottom">
-                    <div class="sold_count">已售出 <span>{{ $product->sold_count }}笔</span></div>
+                    <div class="sold_count">已售出 <span>{{ $product->sold_count }}個</span></div>
                     <div class="review_count">評價 <span>{{ $product->review_count }}</span></div>
                   </div>
                 </div>
