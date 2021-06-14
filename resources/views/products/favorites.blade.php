81be4a52fdb46d8f6abf26e80c79381d85163f2e
tommy <tommy3321@live.com>
2021-06-14 23:38:05 +0800

    統一我的收藏版面

--- resources/views/products/favorites.blade.php
+++ resources/views/products/favorites.blade.php
@@ -2,35 +2,31 @@
 @section('title', '我的收藏')
 
 @section('content')
-  <div class="row">
-    <div class="col-lg-10 offset-lg-1">
-      <div class="card">
-        <div class="card-header">我的收藏</div>
-        <div class="card-body">
-          <form action="{{ route('products.index') }}">
-            <div class="product-list-header">
-            </div>
-          </form>
-          <div class="products-list">
-            @foreach($products as $product)
-              <div class="product-item">
-                <div class="product-content">
-                  <div class="product-img">
-                    <a href="{{ route('products.show', ['product' => $product->id]) }}">
-                      <img src="{{ $product->image_url }}" alt="">
-                    </a>
-                  </div>
-                  <div class="title">
-                    <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
-                  </div>
-                  <div class="price"><b>$</b>{{ $product->price }}</div>
-                </div>
-              </div>
-            @endforeach
+    <div class="card">
+      <div class="card-header">我的收藏</div>
+      <div class="card-body">
+        <form action="{{ route('products.index') }}">
+          <div class="product-list-header">
           </div>
-          <div class="products-page">{{ $products->render() }}</div>
+        </form>
+        <div class="products-list">
+          @foreach($products as $product)
+            <div class="product-item">
+              <div class="product-content">
+                <div class="product-img">
+                  <a href="{{ route('products.show', ['product' => $product->id]) }}">
+                    <img src="{{ $product->image_url }}" alt="">
+                  </a>
+                </div>
+                <div class="title">
+                  <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
+                </div>
+                <div class="price"><b>$</b>{{ $product->price }}</div>
+              </div>
+            </div>
+          @endforeach
         </div>
+        <div class="products-page">{{ $products->render() }}</div>
       </div>
     </div>
-  </div>
 @endsection
